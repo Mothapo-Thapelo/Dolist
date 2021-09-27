c3b3717304427267672d8086c7183ece09922aa1
Mothapo-Thapelo <dev.mothapotr@gmail.com>
2021-09-27 12:52:34 +0200

    added working initial project phase

---
+++ app.js
@@ -0,0 +1,39 @@
+
+        let do_list = [];
+
+        function addItem(){
+
+            //temporarily hold list items
+            
+            //display box for list
+            let display_box = document.querySelector(".item-list");
+
+            //new list item
+            let new_list_item = document.querySelector("#new-item").value;
+
+
+            //check if the new item is not empty or already entered
+            if(new_list_item !== "" && !(do_list.includes(new_list_item))){
+                do_list.push(new_list_item);
+
+                //create a new elements
+                let checkbox = document.createElement("INPUT");
+                checkbox.setAttribute("type", "checkbox");
+
+                let label = document.createElement("label");
+                label.innerText = new_list_item;
+
+                let line_break = document.createElement("br");
+
+                let ruler  = document.createElement("hr");
+
+                display_box.append(checkbox, label, line_break, ruler);
+            
+                //clear the input
+                document.querySelector("#new-item").value = "";
+            }else if (new_list_item === "") alert("Cannot add empty task");
+            else if (do_list.includes(new_list_item)) alert("Task already exists");
+
+ 
+          
+        }
